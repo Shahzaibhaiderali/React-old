d838ce1c45a5713cda013279463cf69cec649afd
Shahzaib <shahziabhaiderali1@gmail.com>
2023-12-28 16:40:33 +0500

    React old process

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import { useState } from 'react';
+import './index.jsx'
+function App() {
+  const  [sum , setNum] = useState(0);
+  function addNum() {
+    setNum(sum + 1);
+    
+  }
+  function subtractNum() {
+    setNum(sum - 1);
+    
+}
+function multiplyNum() {
+  setNum(sum * 1);
+  
+}
+return (
+  <div>
+    <h1>Value of Number</h1>
+    <h2>{sum}</h2>
+    <button onClick={addNum}>ADD +</button>
+    <button onClick={subtractNum}>SUBTRACT -</button>
+</div>
+)
+}
+
+export default App;
